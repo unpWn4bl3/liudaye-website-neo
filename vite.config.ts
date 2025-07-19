c81072c9394de4013062799de5c96b80b141c37a
unpWn4bl3 <smiled1999@gmail.com>
2025-07-20 01:00:32 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
     plugins: [
         tsConfigPaths(),
-        tanstackStart({ customViteReactPlugin: true }),
+        tanstackStart({ customViteReactPlugin: true, target: 'vercel' }),
         viteReact(),
     ],
 })
